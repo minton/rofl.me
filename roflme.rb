7713f43f8cf3591e1c393d715efea9f0d6a1b2e5
Michael Minton <michaelcminton@gmail.com>
2013-01-31 09:03:50 -0500

    Testing other snuff.

--- roflme.rb
+++ roflme.rb
@@ -16,6 +16,53 @@ class Roflme < Sinatra::Base
     doc.css("a.photo:not(.left) img").map{|img| {"rofl" => img["src"].gsub("medium", "large")}}
   end
 
+  def others
+    imgs = ['http://i.imgur.com/ndF9F0t.jpg',
+            'http://i.imgur.com/DmcjGkc.jpg',
+            'http://i.imgur.com/NW03x4N.jpg',
+            'http://i.imgur.com/nKBupwC.jpg',
+            'http://i.imgur.com/GS7f7OD.jpg',
+            'http://i.imgur.com/PELjnWv.jpg',
+            'http://i.imgur.com/ILk4lq9.jpg',
+            'http://i.imgur.com/iIft4tX.jpg',
+            'http://i.imgur.com/4PpmHzs.jpg',
+            'http://i.imgur.com/oDzsGcq.jpg',
+            'http://i.imgur.com/SyU7aMA.jpg',
+            'http://i.imgur.com/ZszSTHz.jpg',
+            'http://i.imgur.com/ffjri10.jpg',
+            'http://i.imgur.com/AmK71N2.jpg',
+            'http://i.imgur.com/XohJAtP.jpg',
+            'http://i.imgur.com/U4OY9oK.jpg',
+            'http://i.imgur.com/bprVQs5.jpg',
+            'http://i.imgur.com/aGm3sTa.jpg',
+            'http://i.imgur.com/cYZQP9U.jpg',
+            'http://i.imgur.com/QaJmMJI.jpg',
+            'http://i.imgur.com/Bd2mPl3.jpg',
+            'http://i.imgur.com/MInBBSq.jpg',
+            'http://i.imgur.com/rvUR8o8.jpg',
+            'http://i.imgur.com/RawISVf.jpg',
+            'http://i.imgur.com/RexkuPV.jpg',
+            'http://i.imgur.com/2Uua94F.jpg',
+            'http://i.imgur.com/MNu5GnT.jpg',
+            'http://i.imgur.com/iSIj0La.jpg',
+            'http://i.imgur.com/ciITCiM.jpg',
+            'http://i.imgur.com/rvE6ufD.jpg',
+            'http://i.imgur.com/F0rLuyV.jpg',
+            'http://i.imgur.com/Dq2f5zl.jpg',
+            'http://i.imgur.com/HQZV0DO.jpg',
+            'http://i.imgur.com/KnxgscE.jpg',
+            'http://i.imgur.com/aXl0t8x.jpg',
+            'http://i.imgur.com/w6A7n8D.jpg',
+            'http://i.imgur.com/EMeBbAz.jpg',
+            'http://i.imgur.com/nYl2eiZ.jpg',
+            'http://i.imgur.com/kMf7RKR.png',
+            'http://i.imgur.com/1niTlRM.jpg',
+            'http://i.imgur.com/7mA3RbY.jpg',
+            'http://i.imgur.com/7mA3RbY.jpg',
+            'http://i.imgur.com/qd2CtKt.jpg']
+    {"rofl" => imgs.sample}
+  end
+
   before do
     content_type :json
   end
@@ -23,8 +70,12 @@ class Roflme < Sinatra::Base
   get '/' do
     rofls.to_json
   end
-
+  get '/poop' do
+    others.to_json
+  end
   get '/random' do
+
     rofls.sample.to_json
   end
+
 end
